23715545639ad7299954a4365867e50bce45aba1
DilinyerCR <dilinyerecr@gmail.com>
2023-03-02 14:23:10 -0400

    cuarto commit

--- main.js
+++ main.js
@@ -29,16 +29,95 @@ setInterval(updateClock, 1000);
 
 
 // !-------------Formulario-------------
-let mensaje = document.getElementById("mensaje-exitoso");
 let enviar = document.getElementById("enviar");
+let enviado = document.getElementById("mensaje-exitoso");
+let error = document.getElementById("mensaje-error");
 
-enviar.addEventListener("click", () => {
-  mensaje.style.display = "block";
+let nombre = document.getElementById("nombre");
+let phone = document.getElementById("phone");
+let email = document.getElementById("email");
+let textMensaje = document.getElementById("textMensaje");
 
-  setTimeout(function() {
-    mensaje.style.display = "none";
-  }, 8000)
+enviar.addEventListener("click" , function validar() {
+  let regExpNombre = /^\s*\S+\s*$/;
+  let regExpPhone = /^\d{10,}$/;
+  let regExpEmail = /^\S+@\S+\.\S+$/;
+  let regExpMensaje = /^\s*\S.*$/;
+
+  if(
+  regExpNombre.test(nombre.value) == true
+  &&
+  regExpPhone.test(phone.value) == true
+  &&
+  regExpEmail.test(email.value) == true
+  &&
+  regExpMensaje.test(textMensaje.value) == true
+  ) {
+    nombre.value = ''
+    phone.value = ''
+    email.value = ''
+    textMensaje.value = ''
+    enviado.style.display = "block";
+    setTimeout(function() {
+      enviado.style.display = "none";
+    }, 4000);
+  } else {
+    error.style.display = "block"
+    setTimeout(function() {
+      error.style.display = "none";
+    }, 2000);
+  };
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let error = document.getElementById("mensaje-error");
+// let enviado = document.getElementById("mensaje-exitoso");
+// let enviar = document.getElementById("enviar");
+
+// let nombre = document.getElementById("nombre");
+
+// enviar.addEventListener("click", function() {
+//   if(nombre.value != /^[a-zA-Z\s]+$/) {
+//     error.style.display = "block";
+//   } else {
+//     enviado.style.display = "block";
+//   }
+
+//   setTimeout(function(){
+//     error.style.display = "none";
+//     enviado.style.display = "none";
+//   }, 2000)
+// });
+
+// enviar.addEventListener("click", () => {
+//   error.style.display = "block";
+
+//   setTimeout(function() {
+//     error.style.display = "none";
+//   }, 5000)
+// });
 
 
 
